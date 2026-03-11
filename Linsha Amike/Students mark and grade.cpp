d8321ca45d186467955b87c93c56ac2c85ebc877
LinshaAmicodes <nfieamaka@gmail.com>
2026-03-11 14:14:46 +0100

    Added two files

---
+++ Linsha Amike/Students mark and grade.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+	int score;
+	printf("Enter student scores: ");// results
+	scanf("%d", &score);
+	
+	if (score >= 80 && score <= 100){// my future marks
+	printf("Grade A");
+}
+    else if(score >= 70){
+        printf("Grade B");
+}
+    else if(score >= 60){
+    	printf("Grade C");
+}
+    else if(score >=50){
+    	printf("Grade D");
+}
+    else{
+    	 printf("Grade F");
+	}
+	
+	return 0;
+    }
